17a04d912d40b85ad643e4be4c903c9ec2361fb9
hvlimbachiya <harshlimbachiya98@gmail.com>
2022-09-21 20:31:07 -0400

    first change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import image from './image.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={image} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          WEB 422: <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <h3>Harsh Vishnu Limbachiya</h3>
       </header>
     </div>
   );
